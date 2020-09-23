cf2cbee03d45f00a062de4f2a2946c95b703a8f2
Ryan McAfee <mcafeeryan92@gmail.com>
2020-09-22 23:46:17 -0400

    Add some stuff for Paula's talk

---
+++ accessible/script.js
@@ -0,0 +1,9 @@
+/* 
+This site lets you find the nearest color to use:
+https://learnui.design/tools/accessible-color-generator.html
+*/
+
+/* 
+Chrome color picker extension
+https://color-picker.freefinancetools.net/
+*/
